f13677a84dd3acaf4787705e256860858c6daccb
Chen <cchengraphic@gmail.com>
2024-04-30 03:30:14 -0700

    set up project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import HomeScreen from "./screens/HomeScreen.jsx";
+import Header from "./components/Header.jsx";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <HomeScreen />
+      </main>
+    </>
+  );
+}
