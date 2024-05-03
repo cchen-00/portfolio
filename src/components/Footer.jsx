2c5b50d8a0330209178cc04af1d3aed5904e3fa0
Chen <cchengraphic@gmail.com>
2024-05-03 00:30:58 -0700

    add content in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,27 @@
+import HeaderIcons from "./header/HeaderIcons";
+import { NAV_ITEMS } from "../constants/constants";
+
 const Footer = () => {
-  return <footer>Footer</footer>;
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="footer">
+      <div className="footer__nav">
+        {NAV_ITEMS.map((item, index) => (
+          <div key={index}>
+            <a href={`/${item.link}`}>{item.name}</a>
+          </div>
+        ))}
+      </div>
+      <div className="footer__icons">
+        <HeaderIcons />
+      </div>
+
+      <div className="footer__copyright">
+        © {currentYear} Ember Chen. All Rights Reserved.
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
