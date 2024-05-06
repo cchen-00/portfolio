1f5fd4b0beb45e8ee1bbf17c72065c8bd1b5855a
Chen <cchengraphic@gmail.com>
2024-05-06 04:55:11 -0700

    Contact UI

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import Input from "./Input";
+import SvgImg from "./SvgImg.jsx";
+import coffee from "../assets/images/coffee.svg";
 
 const validateEmail = (email) => {
-  // Simple email validation
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
@@ -18,27 +19,47 @@ const Contact = () => {
   };
 
   return (
-    <div id="contact" className="contact">
-      <form onSubmit={handleSubmit}>
-        <Input
-          value={email}
-          label="Email"
-          validation={validateEmail}
-          warning="Please enter a valid email."
-          type="input"
-          required
-          onChange={setEmail}
+    <div id="contact">
+      <div className="contact">
+        <div class="contact__text">
+          <h2>Contact</h2>
+          <p>
+            Whether you're interested in collaborating on a project, seeking
+            insights into my work, or simply want to connect, I'm always happy
+            to hear from you. Feel free to reach out via email, social media, or
+            leave a message below.
+          </p>
+        </div>
+
+        <div className="contact__form">
+          <form onSubmit={handleSubmit}>
+            <Input
+              value={email}
+              label="Email"
+              validation={validateEmail}
+              warning="Please enter a valid email."
+              type="input"
+              required
+              onChange={setEmail}
+            />
+            <Input
+              value={message}
+              label="Message"
+              type="textarea"
+              rows={6}
+              onChange={setMessage}
+              required
+            />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+        <SvgImg
+          src={coffee}
+          width="300px"
+          alt="Coffee"
+          className="contact__img"
         />
-        <Input
-          value={message}
-          label="Message"
-          type="textarea"
-          rows={6}
-          onChange={setMessage}
-          required
-        />
-        <button type="submit">Submit</button>
-      </form>
+      </div>
     </div>
   );
 };
